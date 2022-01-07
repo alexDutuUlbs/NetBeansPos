28d2f25828e0c31be945432d35a7e120083153f7
Alex <alexandru.dutu@ausy.com>
2022-01-07 22:24:34 +0200

    test commit after merge

--- src/main/java/servlet/NewOrder.java
+++ src/main/java/servlet/NewOrder.java
@@ -24,6 +24,6 @@ public class NewOrder extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+        //request.get
     }
 }
